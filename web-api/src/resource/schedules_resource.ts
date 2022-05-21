6f0030de9b748754c7b27d74a8531a36e06f66f3
IkedaAkihira <daiki.ikep.0729@gmail.com>
2022-05-21 23:35:54 +0900

    lint

--- web-api/src/resource/schedules_resource.ts
+++ web-api/src/resource/schedules_resource.ts
@@ -1,19 +1,19 @@
-import express from "express";
-import connection,{idgen} from "../db";
-import { badRequestException, notFoundException } from "../error";
-import { getSchedules } from "../service/schedules_service";
-import { checkSessionAndGetUser } from "../service/users_service";
+import express from 'express';
+import {getSchedules} from '../service/schedules_service';
+import {checkSessionAndGetUser} from '../service/users_service';
 
+// eslint-disable-next-line new-cap
 const router=express.Router();
-router.get("/",async (req:express.Request,res:express.Response,next)=>{
-    let user;
-    try {
-        user = await checkSessionAndGetUser(req.session);
-    } catch (e) {
-        return next(e);
-    }
-    const schedules=await getSchedules(user);
-    res.send({schedule:schedules});
+// eslint-disable-next-line new-cap
+router.get('/', async (req:express.Request, res:express.Response, next)=>{
+  let user;
+  try {
+    user = await checkSessionAndGetUser(req.session);
+  } catch (e) {
+    return next(e);
+  }
+  const schedules=await getSchedules(user);
+  res.send({schedule: schedules});
 });
 
 export default router;
--- web-api/src/service/schedules_service.ts
+++ web-api/src/service/schedules_service.ts
@@ -2,32 +2,36 @@ import connection from '../db';
 import {User} from './users_service';
 
 const getCompanyNameById=async (id:BigInt)=>{
-    const companyDatas=await connection().then((c)=>{
-        const datas = c.query('SELECT name FROM companies where id='+id);
-        c.end();
-        return datas;
-    });
-    if (!companyDatas || companyDatas.length == 0||!companyDatas[0].name) {
-        return null;
-    }
-    return companyDatas[0].name;
-}
+  const companyDatas=await connection().then((c)=>{
+    const datas = c.query('SELECT name FROM companies where id='+id);
+    c.end();
+    return datas;
+  });
+  if (!companyDatas || companyDatas.length == 0||!companyDatas[0].name) {
+    return null;
+  }
+  return companyDatas[0].name;
+};
 
 export const getSchedules=async (user:User)=>{
-    const scheduleDatas=await connection().then((c)=>{
-        const datas=c.query('SELECT id,title,company_id,url,note,start,end FROM schedules WHERE user_id=?',[user.id]);
-        c.end();
-        return datas;
-    });
-    const schedules:object[]=[];
-    for(const data of scheduleDatas){
-        const company_name:string=await getCompanyNameById(data.company_id);
-        if(!company_name){
-            continue;
-        }
-        const schedule:object={company_name:company_name};
-        Object.assign(schedule,data);
-        schedules.push(schedule);
+  const scheduleDatas=await connection().then((c)=>{
+    const datas=c.query(
+        'SELECT id,title,company_id,url,note,start,end' +
+            'FROM schedules WHERE user_id=?'
+        , [user.id],
+    );
+    c.end();
+    return datas;
+  });
+  const schedules:object[]=[];
+  for (const data of scheduleDatas) {
+    const companyName:string=await getCompanyNameById(data.company_id);
+    if (!companyName) {
+      continue;
     }
-    return schedules;
-}
+    const schedule:object={company_name: companyName};
+    Object.assign(schedule, data);
+    schedules.push(schedule);
+  }
+  return schedules;
+};
