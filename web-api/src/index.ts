84d4713d2923c6b4afb63862be7a175e4fddecea
Nameless <tanisyun0331@icloud.com>
2022-05-22 10:25:09 +0900

    cros対応(仮)

--- web-api/src/index.ts
+++ web-api/src/index.ts
@@ -27,14 +27,18 @@ app.use(session(sessionOpt));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-// CROS対応（とりあえず全許可）
+// CROS対応
 app.use(
     (req: express.Request,
         res: express.Response,
         next: express.NextFunction) => {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', '*');
-      res.header('Access-Control-Allow-Headers', '*');
+      res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+      res.header('Access-Control-Allow-Methods', 'POST, GET, DELETE, PUT');
+      res.header(
+          'Access-Control-Allow-Headers',
+          'X-Requested-With, Origin, X-Csrftoken, Content-Type, Accept',
+      );
+      res.header('Access-Control-Allow-Credentials', 'true');
       next();
     },
 );
