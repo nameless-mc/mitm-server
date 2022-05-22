cdc2697e470d25daa32cf772cd32fc88a1c839d7
IkedaAkihira <daiki.ikep.0729@gmail.com>
2022-05-22 14:46:58 +0900

    スケジュールAPIの全機能実装

--- web-api/src/resource/schedules_resource.ts
+++ web-api/src/resource/schedules_resource.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import {getSchedules} from '../service/schedules_service';
+import {getSchedules,getScheduleById,createSchedule,deleteSchedule,updateSchedule} from '../service/schedules_service';
 import {checkSessionAndGetUser} from '../service/users_service';
 
 // eslint-disable-next-line new-cap
@@ -18,15 +18,110 @@ router.get('/', async (req:express.Request, res:express.Response, next)=>{
 });
 
 router.get('/:id', async (req:express.Request, res:express.Response, next)=>{
-  let user,schedules;
+  let user,schedule;
   const id=req.params.id;
   try {
     user = await checkSessionAndGetUser(req.session);
-    schedules = await getSchedules(user);
+    schedule = await getScheduleById(parseInt(id));
   } catch (e) {
     return next(e);
   }
-  res.send({schedule: schedules});
+  res.send(schedule);
 });
+
+router.post('/',async (req:express.Request,res:express.Response,next)=>{
+  let user;
+  try {
+    user = await checkSessionAndGetUser(req.session);
+  } catch (e) {
+    return next(e);
+  }
+
+  try {
+    const schedule = await createSchedule(user, {
+      title: req.body.title,
+      companyId: req.body.company_id,
+      companyName: req.body.company_name,
+      url:req.body.url,
+      note: req.body.note,
+      start: req.body.start,
+      end: req.body.end
+    }).catch((e) => {
+      throw next(e);
+    });
+
+    res.send({
+      id: schedule.id,
+      title: schedule.title,
+      company_id: schedule.company_id,
+      company_name: schedule.company_name,
+      url:schedule.url,
+      note: schedule.note,
+      start: schedule.start,
+      end: schedule.end
+    });
+  } catch (e) {
+    next(e);
+  }
+});
+
+
+
+router.put(
+  '/:scheduleId',
+  async (req: express.Request, res: express.Response, next) => {
+    const scheduleId = req.params.scheduleId;
+    let user;
+    try {
+      user = await checkSessionAndGetUser(req.session);
+    } catch (e) {
+      return next(e);
+    }
+    try {
+      const schedule = await updateSchedule(user, {
+        id: parseInt(scheduleId),
+        title: req.body.title,
+        companyId: req.body.company_id,
+        companyName: req.body.company_name,
+        url:req.body.url,
+        note: req.body.note,
+        start: req.body.start,
+        end: req.body.end
+      });
+
+      res.send({
+        id: schedule.id,
+        title: schedule.title,
+        company_id: schedule.company_id,
+        company_name: schedule.company_name,
+        url:schedule.url,
+        note: schedule.note,
+        start: schedule.start,
+        end: schedule.end
+      });
+    } catch (e) {
+      return next(e);
+    }
+  },
+);
+
+router.delete(
+  '/:scheduleId',
+  async (req: express.Request, res: express.Response, next) => {
+    const scheduleId = req.params.scheduleId;
+    let user;
+    try {
+      user = await checkSessionAndGetUser(req.session);
+    } catch (e) {
+      return next(e);
+    }
+    try {
+      await deleteSchedule(user, parseInt(scheduleId));
+      res.status(204).send();
+    } catch (e) {
+      return next(e);
+    }
+  },
+);
 
 export default router;
--- web-api/src/service/schedules_service.ts
+++ web-api/src/service/schedules_service.ts
@@ -1,6 +1,6 @@
-import connection from '../db';
+import connection, { idgen } from '../db';
 import {User} from './users_service';
-import { notFoundException } from '../error';
+import { badRequestException, notFoundException } from '../error';
 
 const getCompanyNameById=async (id:BigInt)=>{
   const companyDatas=await connection().then((c)=>{
@@ -57,4 +57,135 @@ export const getScheduleById=async (id:number)=>{
   const schedule:object={company_name: companyName};
   Object.assign(schedule, scheduleDatas[0]);
   return schedule;
+}
+
+export const createSchedule=async (
+  user:User,
+  data:{
+      title: string,
+      companyId: number,
+      companyName: string,
+      url?:string,
+      note?: string,
+      start: Date,
+      end: Date
+  }
+)=>{
+  const schedule={
+    id:idgen(),
+    user_id:user.id,
+    title:data.title,
+    company_id:data.companyId,
+    url:data.url,
+    note:data.note,
+    start:data.start,
+    end:data.end,
+    updated_at:new Date(),
+    created_at:new Date()
+  };
+  const res=await connection().then((c)=>{
+    const data=c.query('INSERT INTO schedules VALUES(?,?,?,?,?,?,?,?,?,?)',
+    [
+      schedule.id,
+      schedule.user_id,
+      schedule.title,
+      schedule.company_id,
+      schedule.url,
+      schedule.note,
+      schedule.start,
+      schedule.end,
+      schedule.updated_at,
+      schedule.created_at
+    ]).catch(()=>{
+      return badRequestException();
+    });
+    c.end();
+    return data;
+  });
+  if(res instanceof Error){
+    throw badRequestException();
+  }
+  return {
+    id:schedule.id,
+    title:schedule.title,
+    company_id:schedule.company_id,
+    company_name:data.companyName,
+    url:schedule.url,
+    note:schedule.note,
+    start:schedule.start,
+    end:schedule.end
+  };
+
+}
+
+export const updateSchedule=async(
+  user:User,
+  data:{
+      id:number,
+      title: string,
+      companyId: number,
+      companyName: string,
+      url?:string,
+      note?: string,
+      start: Date,
+      end: Date
+  }
+)=>{
+  const oldData:any=await getScheduleById(data.id);
+  const schedule={
+    id:data.id,
+    user_id:user.id,
+    title:data.title||oldData.title,
+    company_id:data.companyId||oldData.company_id,
+    company_name:data.companyName||oldData.company_name,
+    url:data.url||oldData.url,
+    note:data.note||oldData.note,
+    start:data.start||oldData.start,
+    end:data.end||oldData.end,
+    updated_at:new Date(),
+    created_at:oldData.created_at
+  };
+  const res = await connection().then((c) => {
+    return c
+        .query(
+            'UPDATE schedules SET ' +
+          'id = ?, user_id = ?, title = ?, company_id = ?' +
+          ', url = ?, note = ?, start = ?, end = ?' +
+          ', updated_at = ?, created_at = ?' +
+          ' WHERE id = ?',
+            [
+              schedule.id,
+              schedule.user_id,
+              schedule.title,
+              schedule.company_id,
+              schedule.url,
+              schedule.note,
+              schedule.start,
+              schedule.end,
+              schedule.updated_at,
+              schedule.created_at
+            ]
+        )
+        .catch(() => {
+          return badRequestException();
+        });
+  });
+  if (res instanceof Error) {
+    throw badRequestException();
+  }
+  return schedule;
+}
+
+export const deleteSchedule=async(user:User,scheduleId:number)=>{
+  const res=await connection().then((c)=>{
+    const data=c.query("DELETE FROM schedules WHERE id=?",
+    [scheduleId]).catch(()=>{
+      return badRequestException();
+    });
+    c.end();
+    return data;
+  });
+  if(res instanceof Error){
+    throw badRequestException();
+  }
 }
